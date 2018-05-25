f6925e44dfa7b5f4b4029997ab5667cfe3f81274
edoardocoen <edoardo.coen@yahoo.it>
2018-05-26 00:56:51 +0300

    Update SkyInstallScriptSecondary.sh

--- SkyInstallScriptSecondary.sh
+++ SkyInstallScriptSecondary.sh
@@ -132,7 +132,7 @@ sleep 5
 cd $GOPATH/bin
 ###### Starts Skywire Monitor service for the first time / after that command web interface of Monitor works
 ###### This line is to start the monitor on the slave nodes (OrangePI 2-8) only / Master Orange PIs requiere a different line!!!
-./node -connect-manager -manager-address 192.168.0.101:5998 -manager-web 192.168.0.101:8000  -address :5000 -web-port :6001 > /dev/null 2>&1 &
+nohup ./node -connect-manager -manager-address 192.168.0.101:5998 -manager-web 192.168.0.101:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &cd /
 echo "Skywire node is now started and you can see it in on the Monitor." 
 
 ##### START SCRIPTS / STOP SCRIPTS
@@ -140,7 +140,7 @@ echo "Skywire node is now started and you can see it in on the Monitor."
 echo "Downloading autostart scripts from TheSKYpeople Github" 
 cd ~
 wget https://raw.githubusercontent.com/TheSKYpeople/SkyInstallScript/master/ServiceStartSkycoinWallet.sh
-wget https://raw.githubusercontent.com/TheSKYpeople/SkyInstallScript/master/ServiceStartSkywireSecondary.sh
+wget https://raw.githubusercontent.com/TheSKYpeople/SkyInstallScript/master/startsecond.sh
 wget https://raw.githubusercontent.com/TheSKYpeople/SkyInstallScript/master/ServiceStopSkywire.sh
 
  
