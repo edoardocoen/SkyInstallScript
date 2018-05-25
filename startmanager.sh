10d094b070f1ccaf577eb94e5cb7b76f53d744f3
edoardocoen <edoardo.coen@yahoo.it>
2018-05-26 01:01:35 +0300

    Update startmanager.sh

--- startmanager.sh
+++ startmanager.sh
@@ -22,4 +22,4 @@ cd $GOPATH/bin
 
 ##### This starts the Skywire Monitor (Primary Node!!!)
 ##### Attention!!! This script is only to be used on Master/ Primary Node (OrangePI 1)!!!
-nohup ./node -connect-manager -manager-address 192.168.0.101:5998 -manager-web 192.168.0.101:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &cd /
+nohup ./node -connect-manager -manager-address 192.168.0.51:5998 -manager-web 192.168.0.51:8000 -discovery-address discovery.skycoin.net:5999-034b1cd4ebad163e457fb805b3ba43779958bba49f2c5e1e8b062482904bacdb68 -address :5000 -web-port :6001 > /dev/null 2>&1 &cd /
